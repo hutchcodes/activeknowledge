93d640ca286eee4facf999bb86d68f35c3fd522a
Jeremy Hutchinson <jrhutch@live.com>
2019-03-28 20:09:00 -0400

    Styling with Ro's styles

--- Resurgam.Admin.Web/wwwroot/js/site.js
+++ Resurgam.Admin.Web/wwwroot/js/site.js
@@ -1,4 +1,28 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+// TOC toggler
+
+$('#tocBtn').click(function () {
+    $("#tocContent").toggleClass('col-lg-9');
+});
+
+// TOC index
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function () {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+    });
+}
+
+// Tooltip activation 
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+//
