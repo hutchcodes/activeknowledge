3ccfd1acad5b2faa6e540f12c0a07fcffe4a8d62
Jeremy Hutchinson <jrhutch@live.com>
2019-10-28 09:39:13 -0400

    Keyvault access for App

--- AKS.App.Build/Program.cs
+++ AKS.App.Build/Program.cs
@@ -9,11 +9,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 
 namespace AKS.App.Build
 {
     public static class Program
     {
+        static AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -27,7 +30,12 @@ namespace AKS.App.Build
                     webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
                     {
                         var settings = config.Build();
-#if !DEBUG
+#if DEBUG
+                        var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                        config.AddAzureAppConfiguration(options => {
+                            options.Connect(settings["ConnectionStrings:AppConfig"])
+                                    .UseAzureKeyVault(kvClient);
+                        });
                         config.AddAzureAppConfiguration(settings["ConnectionStrings:AppConfig"]);
 #endif
                     });
